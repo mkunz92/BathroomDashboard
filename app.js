d7e1dd3c3229dcc6a44f8d3b36f5f7ee13d409fd
mkunz92 <mkunzgm@gmail.com>
2026-03-18 19:59:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -167,7 +167,6 @@ function updateBackgroundMode() {
   video.style.transition = "opacity 1.5s ease";
 
   if (hour >= 0 && hour < 7) {
-    // 🌙 Nacht
     document.body.classList.remove("day-mode");
     document.body.classList.add("night-mode");
 
@@ -179,9 +178,7 @@ function updateBackgroundMode() {
       video.classList.add("hidden");
       video.pause();
     }, 1500);
-
   } else {
-    // 🌞 Tag
     document.body.classList.remove("night-mode");
     document.body.classList.add("day-mode");
 
